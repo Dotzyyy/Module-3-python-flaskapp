77f06f153398875d9c1d2719836a157e3dd4ab29
David Sexton <davidsexton36@gmail.com>
2024-02-12 20:03:35 +0000

    css files added

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-
+posts = [
+        {
+          'author': 'David Sexton',
+           'title': 'Blog Post 1',
+            'content': 'First post' ,
+            'date': '10/01/24',
+        },
+        {
+          'author': 'David Sexton',
+           'title': 'Blog Post 2',
+            'content': 'Second post' ,
+            'date': '12/01/24',
+        },
+]
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return "<h1>Home Page</h1>"
+    return render_template('index.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__mainn__':
         app.run(debug=True)
