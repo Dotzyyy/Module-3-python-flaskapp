d367901f8eed1f2314ac5e5eb328b83e63dd17a7
David Sexton <davidsexton36@gmail.com>
2024-02-19 20:25:07 +0000

    Finishing up

--- static/js/script.js
+++ static/js/script.js
@@ -2,8 +2,11 @@
 
 
 // Collapse section
+
+// selecting the container for each faq
   let faqs = document.querySelectorAll(".faq");
 
+//toggling the css class 'active' in order to use accordian
   faqs.forEach(faq => {
     faq.addEventListener("click", () => {
       faq.classList.toggle("active")
@@ -21,9 +24,10 @@
 
 
   
-
+  // A more elegant decision than last time. Instead of removing and adding classes, merely toggle the 'dark-mode class'
   colorIcon.addEventListener("click", function(){
     document.body.classList.toggle("dark-mode");
+    // code to switch the icon taht changes theme depending if 'dark mode' is on or not
     if(document.body.classList.contains("dark-mode")) {
       colorIcon.src = "static/sun.png"
     } else{
